6ce369dbe98c5a2a1d0b7361bd80aaa3f57f63ee
Jose <jose.mesar@udea.edu.co>
2025-12-01 22:52:31 -0500

    Quitando el segundo panel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from '@/components/Organisms/Layout';
-import { usePathname } from 'next/navigation';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '@/components/Organisms/Layout'
+import { usePathname } from 'next/navigation'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const pathname = usePathname();
-  // Exclude Layout for login page
+  const pathname = usePathname()
+
+  // Excluir el Layout solo en la página de login
   if (pathname === '/login') {
-    return <Component {...pageProps} />;
+    return <Component {...pageProps} />
   }
+
+  // Todas las demás páginas se renderizan con Layout (que ya incluye AppSidebar)
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
+
