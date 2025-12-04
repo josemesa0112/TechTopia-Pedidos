985b5514a23ca11996aaeafd683ab279267bf38e
Jose <jose.mesar@udea.edu.co>
2025-12-03 22:42:07 -0500

    Organizando pagina principal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '@/components/Organisms/Layout'
 import { usePathname } from 'next/navigation'
 
 export default function App({ Component, pageProps }: AppProps) {
